4ac7fa3634bb3e5b2aa6063f991f0c393c672a10
Hansen Marcelino Azali <hansenmarcelino54@gmail.com>
2022-08-02 13:14:42 +0700

    adssa

--- map/map.go
+++ map/map.go
@@ -4,11 +4,11 @@ import "fmt"
 
 //map adalah tipe kumpulan data lain yang berisi data yang tipe data sama
 
-func main()  {
+func main() {
 	//untuk membuat map harus mendifinisikan tipe data key dan tipe data valuenya
-	person:=map[string]string{ 
-		"name": "hansen",
-		"address" : "Yogyakarta",
+	person := map[string]string{
+		"name":    "hansen",
+		"address": "Yogyakarta",
 	}
 
 	fmt.Println(person)
@@ -16,5 +16,19 @@ func main()  {
 	fmt.Println(person["address"])
 
 	person["title"] = "programmer"
-	fmt.Println()
+	fmt.Println("ini adalah person : ", person)
+
+	var chickens = []map[string]string{
+		{"name": "chicken blue", "gender": "male"},
+		{"name": "chicken red", "gender": "male"},
+		{"name": "chicken yellow", "gender": "female"},
+	}
+	fmt.Println("ini adalah chiken : ", chickens)
+
+	var data = []map[string]string{
+		{"name": "chicken blue", "gender": "male", "color": "brown"},
+		{"address": "mangga street", "id": "k001"},
+		{"community": "chicken lovers"},
+	}
+	fmt.Println("ini adalah data:", data)
 }
